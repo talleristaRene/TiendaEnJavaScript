f5b24f20bf05a2279ed6f80c9b805d266ad7b5a4
Rene PW <tallerista.pilares.rene@gmail.com>
2021-11-07 01:57:52 -0600

    Primer commit

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+
+// alert(hoy.toDateString);
+
+var d = new Date();
+document.write( '<footer class="footer">');
+    document.write( '<p class="foo__text">Todos los derechos Reservados Normandy Wings</p>');
+    document.write( '<p class="foo__text">'+ 
+                        '<h3>'  + 
+                                d.getDate() +'/' + d.getMonth() + '/' + d.getFullYear() +
+                        '</h3>' +
+                        '<h3>'  + 
+                                d.getHours() +':' + d.getMinutes() + ':' + d.getSeconds()+
+                        '</h3>' +
+                    '</p>');
+    // document.write( '<p class="foo__text">'+ '<h3>' + d.getHours() +':' + d.getMinutes() + ':' + d.getSeconds() +'</h3>'+'</p>');
+document.write( '</footer>');
+
