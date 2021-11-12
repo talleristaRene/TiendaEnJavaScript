a1b2dbbe5dbffd837f2a985c549ea8c7c9f4a7be
Rene PW <tallerista.pilares.rene@gmail.com>
2021-11-12 12:05:43 -0600

    Finalizado

--- public/js/pedido.js
+++ public/js/pedido.js
@@ -1,95 +1,33 @@
-/*const arreglo={
-    nombre: '', precio: null
-}*/
+let productos = [];
+let total = 0;
 
-
-
-
-
-
-/*function productos(){
-    var valorProductos = document.formulario.nombre.value;
-    arreglo.push(valorProductos);
-    console.log(arreglo);
-}*/
-// const formulario = document.querySelector('.formulario');
-// const nombre = document.querySelector('#nombre');
-// const precio = document.querySelector('#precio');
-
-// nombre.addEventListener('input', leerTexto);
-// precio.addEventListener('input', leerTexto);
-
-// function leerTexto(evento) {
-//     arreglo[evento.target.id] = evento.target.value;
-//     console.log(arreglo);
-// }
-
-// formulario.addEventListener('click', function(evento){
-//     evento.preventDefault();
-//     nombre.addEventListener('input', leerTexto);
-//     precio.addEventListener('input', leerTexto);
-// });
-
-// var i = 0
-// formulario.addEventListener('click', function(evento){
-//         evento.preventDefault();
-//         // condicion solo 10 productos
-//         if( i < 10 ){
-//             arreglo[evento.target.id] = evento.target.value;
-//             console.log(arreglo);
-//         }        
-//     i++;
-// });    
-
-    
-
-
-
-
-
-// formulario.addEventListener('submit', function(evento){
-//     evento.preventDefault();
-//     // arreglo.push({
-//     //     nombre: document.getElementById("nombre").value, precio: document.getElementById("precio").value
-//     // })
-//     // Validaciones
-//     console.log('Enviando Formulario');
-// })
-
-
-
-
+/**
+ * Funcion que va sumando los valores del arreglo para obbtener el Costo Total.
+ */
+function generarTotal(){
+    let suma = 0;
+    for(let i = 0; i < productos.length; i++){
+        suma += productos[i].precio;
+    }
+    total = suma;
+}
 /**
  * Funcion que permite escribir los valores que se ingresan al formulario mediante API Web
  */
  function escribe() {
-    console.log(document.getElementById("nombre").value);   
-    console.log(document.getElementById("precio").value);  
-
-     var idNombre;
-    var idNombre;
-    var i = 0;
-//     // Forma A
-//     arreglo.push({
-//         nombre: document.getElementById("nombre").value, precio: document.getElementById("precio").value
-//     })
-//     // Forma B
-//     // for(i=0;i<5;i++){
-//     //     idNombre = "nombre" + (i +1);
-//     //     idPrecio = "precio" + (i +1);
-//     //     arreglo.push({
-//     //         nombre: document.getElementById(idNombre).value, precio: document.getElementById(idPrecio).value
-//     //     })
-//     // }
-//     console.log(arreglo);
-    
-     escribir = document.getElementById("caja");
-     miTitulo = "<h1>" + "Caja" +"</h1>";
-     totalProductos = "<h3>" + "Total de Productos: " + "<p>"+ i + "</p>"  + "</h3>";
-     costoTotal = "<h3>" + "Costo Total : " + "<p>"+ i + "</p>"  + "</h3>";
-    
-     contenedor = '<div class="contenedor__formulario">' + miTitulo + nombre + precio  + "</div>";
-     escribir.innerHTML = contenedor;
+    var nombreP =  document.getElementById("nombre").value;   
+    var precioP =  parseInt( document.getElementById("precio").value ) ; 
+    console.log(precioP);
+    productos.push({
+        nombre: nombreP, precio: precioP
+    });
+    generarTotal();
+    escribir = document.getElementById("caja");
+    miTitulo = "<h1>" + "Caja" +"</h1>";
+    totalProductos = "<h3>" + "Cantidad de Productos: " + "<p>"+ productos.length + "</p>"  + "</h3>";
+    costoTotal = "<h3>" + "Costo Total : " + "<p>"+ total + "</p>"  + "</h3>";
+    contenedor = '<div class="contenedor__formulario">' + miTitulo + totalProductos + costoTotal  + "</div>";
+    escribir.innerHTML = contenedor;
 }
 
 window.onload = function() {
