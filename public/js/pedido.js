a6b2740def5f99537430b0c87dff27dae5d5e241
Rene PW <tallerista.pilares.rene@gmail.com>
2021-11-14 12:16:32 -0600

    Se agrega commit para cambiar Objeto por Clase

--- public/js/pedido.js
+++ public/js/pedido.js
@@ -1,27 +1,66 @@
 'use strict';
-import {Producto} from './producto.js';
 
 let productos = [];
 let total = 0;
 
-var producto1  = new Producto("Papas", 24.3);
-let producto2  = new Producto("Alitas", 24.3);
-let producto3  = new Producto("Papas", 24.3);
+// Objeto producto
+function Producto(nombre, precio, porcion){
+    // atributos
+    this.nombre = nombre; 
+    this.precio = precio;
+    this.porcion = porcion;
+    // Metodos
+    this.getNombre = function(){return this.nombre;}
+    this.getPrecio = function(){return precio;},
+    this.getPorcion = function(){return porcion;},
+    this.setNombre = function(nombre){this.nombre = nombre;},
+    this.setPrecio = function(precio){this.precio = precio;},
+    this.setPorcion = function(porcion){this.porcion = porcion;}
+}
+
+var p1 = new Producto("Papas", 26, 35);
+var p2 = new Producto("Alitas", 45, 6);
+var p3 = new Producto("Boneles", 30, 10);
+var p4 = new Producto("Aros de Cebolla", 30, 10);
 
 
-var catalogo = {
-    nombre: producto1.getNombre(), precio: producto1.getPrecio(),
+
+// console.log(p1.getPrecio());
+// var Producto = {
+//     // atributos
+//     nombre: " " , precio: 0, porcion: 0,
+//     // Metodos
+//     getNombre: function(){return nombre;},
+//     getPrecio: function(){return precio;},
+//     getPorcion: function(){return porcion;},
+//     setNombre(nombre){this.nombre = nombre;},
+//     setPrecio(precio){this.precio = precio;},
+//     setPorcion(porcion){this.porcion = porcion;},
+    
+// }
+
+
+var Catalogo = {
+    productos:[],
+    agregarProducto: function(producto){
+        if(!this.productos){
+         this.productos = [producto]
+        } else {
+         this.productos.push(producto);
+        }
+      },
+    obtenerPrecioTotal: function(){
+    return this.productos.reduce((total, p) => total + p.precio, 0);
+    }
 };
+// const catalogo = Object.create(Catalogo);
 
-catalogo.push({
-    "nombre": "producto2.getNombre()", "precio": "producto2.getPrecio()"
-});
+// catalogo.agregarProducto(p1);
+// catalogo.agregarProducto(p2);
+// catalogo.agregarProducto(p3);
+// catalogo.agregarProducto(p4);
 
-
-
-
-console.log(catalogo);
-
+// console.log(catalogo.obtenerPrecioTotal());
 
 // for (let i = 0; i < catalogo.length; i++) {
 //     console.log(catalogo[i]);
