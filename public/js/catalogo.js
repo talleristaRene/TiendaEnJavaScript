d74d73f971b51f5cd6aeba98ac9955fd85a4d476
Rene PW <tallerista.pilares.rene@gmail.com>
2021-11-15 19:17:44 -0600

    Version final de la actividad

--- public/js/catalogo.js
+++ public/js/catalogo.js
@@ -8,6 +8,9 @@ var Catalogo = {
          this.productos.push(producto);
         }
       },
+    setProductos:function(listaProductos){
+        this.productos = listaProductos;
+    },
     obtenerPrecioTotal: function(){
     return this.productos.reduce((total, p) => total + p.precio, 0);
     },
@@ -15,6 +18,12 @@ var Catalogo = {
     getProductos: function(){
         let prod = this.productos;
         return prod;
+    }, 
+    mostrarProductos: function(){
+        for (let index = 0; index < this.productos.length; index++) {
+            // const element = array[index];
+            console.log(this.productos[index]);
+        }
     }
 };
 
@@ -43,58 +52,109 @@ class Producto{
     getPrecio(){
         return this.precio;
     }
+    setNombre(nombre){
+        this.nombreProducto = nombre;
+    }
 }
 
 function main() {
-    let arrProductos = [];
-    var producto1 = new Producto("Papas", 100);
-    var producto2 = new Producto("Alitas", 50);
-    var producto3 = new Producto("Boneles", 70);
-    var producto4 = new Producto("Aros de Cebolla", 100);
+    let arrProductos    = [];
+    var catalogo        =  Object.create(Catalogo);
+    var producto1 = new Producto("Papas", 100,23);
+    var producto2 = new Producto("Alitas", 50,64);
+    var producto3 = new Producto("Boneles", 70,34);
+    var producto4 = new Producto("Aros de Cebolla", 100,20);
     arrProductos.push(producto1);
-    arrProductos.push(producto2);
     arrProductos.push(producto3);
+    arrProductos.push(producto2);
     arrProductos.push(producto4);
-    console.log("El arreglo de Productos Tiene :" + arrProductos.length + " productos");
+    
+    catalogo.setProductos(arrProductos);
+    var listaOrdenada = catalogo.getProductos().sort(ordenarAlfabeticamente);
+ 
+    var suma = sumarPrecioProductos(catalogo.productos);
+    console.log(suma);
 }
 
+/**
+ * Funcion que escribe los productos del catalogo en una tabla
+ */
 function escribe() {
+
     var producto; 
     var catalogo    =  Object.create(Catalogo);
     var nombreP     =  document.getElementById("nombre").value;   
-    var existenciaP = document.getElementById("existencia").value;
-    var precioP     = document.getElementById("precio").value;
-    producto        = new Producto(nombreP, existenciaP, precioP);
-    catalogo.agregarProducto(producto);
+    var existenciaP =  document.getElementById("existencia").value;
+    var precioP     =  document.getElementById("precio").value;
+     
 
-    var contenedor = document.getElementById("contenedor__tabla");
-    var tabla = '<table class="cabeza-tabla">';
+    if( esNumero(precioP) ){
+        producto        = new Producto(nombreP, existenciaP, precioP);
+    }
+    catalogo.agregarProducto(producto);
     
-    tabla += '<thead>'+
+    var contenedor = document.getElementById("contenedor__tabla");
+    var tabla = '<table class="tabla">';
+    
+    tabla += '<thead class="tabla__cabecera">'+
                 '<tr>  '+
-                    '<th scope="col" >'+"<p>"+"Nombre"+"</p>"+'</th>'+
-                    '<th scope="col" >'+"<p>"+"Existencia"+"</p>"+'</th>'+
-                    '<th scope="col" >'+"<p>"+"Precio"+"</p>"+'</th>'+
+                    '<th scope="col" id="cabecera__nombre">'+"<p>"+"Nombre"+"</p>"+'</th>'+
+                    '<th scope="col" id="cabecera__existencia">'+"<p>"+"Existencia"+"</p>"+'</th>'+
+                    '<th scope="col" id="cabecera__precio">'+"<p>"+"Precio"+"</p>"+'</th>'+
                 '</tr>'+
             '</thead>';
+
+    var exp = eval(catalogo);
+    console.log(  eval(catalogo).getProductos() );
     
+    var exp = "";
+    var listaProductos = catalogo.getProductos();
     for (let i = 0; i < catalogo.getLengthProductos(); i++) {
-        var listaProductos = catalogo.getProductos();
-        tabla +=    "<tbody>"+
+        exp += eval(catalogo).getProductos()[i].getNombre();
+        console.log("Nombre como una sola cadena: " + exp);
+        
+        tabla +=    '<tbody class="tabla__cuerpo"'+
                         "<tr>"+
-                            '<th scope="col">'+"<p>"+listaProductos[i].getNombre()+"</p>"+'</th>'+
-                            '<th scope="col">'+"<p>"+listaProductos[i].getExistencia()+"</p>"+'</th>'+
-                            '<th scope="col">'+"<p>"+listaProductos[i].getPrecio()+"</p>"+'</th>'+
+                            '<th scope="col">'+"<p>"+listaProductos[i].getNombre()   +"</p>"+'</th>'+
+                            '<th scope="col">'+"<p>"+listaProductos[i].getExistencia()+ "</p>"+'</th>'+
+                            '<th scope="col">'+"<p>"+listaProductos[i].getPrecio()+ "</p>"+'</th>'+
                         "</tr>"+
                     "</tbody>";
-        tabla += "</head>"
+        tabla += "</head>";
     }
     tabla += '</table>';
 
-    var cont = '<div class="contenedor__formulario">' + tabla +"</div>";
+    var cont =  '<div class="contenedor__formulario">'+
+                    tabla+
+                '</div>';
+
     contenedor.innerHTML = cont;
 }
 
+
+function esNumero(valor){
+    var valido = false;
+    if( !isNaN(valor) ){
+        valido = true;
+    }else{
+        alert("Debes de ingresar un valor numerico");
+    }
+    return valido;
+}
+
+function ordenarAlfabeticamente(productoA,productoB){
+    if (productoA.nombreProducto < productoB.nombreProducto) {return -1;}
+    if (productoA.nombreProducto > productoB.nombreProducto) {return 1;}
+    return 0;
+}
+function sumarPrecioProductos(listaP){
+    if(listaP.length == 1){
+        var elemento = listaP[0];
+        return elemento.getPrecio();
+    }else{
+        return listaP.pop().getPrecio() + sumarPrecioProductos(listaP);
+    }
+}
 window.onload = function() {
 document.rellenar.ver.onclick = escribe
 }   
