74442605e083fa81fda4a0187ce13f0dcecd7c4d
Rene PW <tallerista.pilares.rene@gmail.com>
2021-12-01 13:29:26 -0600

    Se agrega el get

---
+++ public/javascripts/validacion.js
@@ -0,0 +1,12 @@
+function mostrarFormulario(tipo){
+    // Agregramos
+    if(tipo == "post"){
+        window.location.href = "/formGet";
+    }
+    if(tipo == "get"){
+        window.location.href = "/formGet";
+    }
+    if(tipo == "post"){
+        window.location.href = "/formPost";
+    }
+}
--- routes/productos.js
+++ routes/productos.js
@@ -1,13 +1,60 @@
 const { Router } = require('express');
 const router = Router();
 const productos = require('../sample.json');
+const _ = require('underscore');
 
 console.log(productos);
 
+// Get
 router.get('/', (req, res) =>{
-    res.json(productos);
+    res.render('almacen', { productos} ); 
+    // res.json(productos);
 });
 
+// Post
+router.post('/', (req, res) =>{
+    const {nombre, precio, cantidad} = req.body;
+    if(nombre && precio && cantidad) {
+        const id = productos.length + 1;
+        const nuevoProducto = {...req.body, id};
+        productos.push(nuevoProducto);
+        res.json(productos);
+    }else{
+        res.send('Peticion erronea');
+    }
+    res.send('recibido');
+});
+
+// Put
+router.put('/:id', (req,res)=>{
+    const {id} = req.params;
+    const {nombre, precio, cantidad} = req.body;
+    if(nombre && precio && cantidad) {
+        _.each(productos, (producto, i) => {
+            if(producto.id == id){
+                producto.nombre = nombre;
+                producto.precio = precio;
+                producto.cantidad = cantidad;
+            }
+        });
+        res.json(productos);
+    }else{
+        res.status(500).json({error: 'Todos los campos son requeridos'});
+    }
+
+});
+
+
+// Delete
+router.delete('/:id', (req,res)=>{
+    const {id} = req.params;
+    _.each(productos, (producto,i) => {
+        if(producto.id == id){
+            productos.splice(i,1);
+        }
+    });
+    res.send(productos);
+});
 
 
 module.exports = router;
